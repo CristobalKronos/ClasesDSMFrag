81703d26a4fa7a67d10b4aaa9800aa0737e2978b
CristóbalO_Kronos <dondoncronos@gmail.com>
2022-08-31 13:26:36 -0400

    Initial commit

---
+++ app/src/main/java/cl/ucn/disc/dsm/clasesdsm/SecondFragment.java
@@ -0,0 +1,56 @@
+package cl.ucn.disc.dsm.clasesdsm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import cl.ucn.disc.dsm.clasesdsm.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        }
+        );
+        binding.thirthbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view1) {
+                NavHostFragment.findNavController(ThirthFragment.this)
+                        .navigate(R.id.toThirth);
+            }
+        }
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
