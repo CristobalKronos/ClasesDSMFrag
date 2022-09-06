2714ccde6c1077cdaf9e318adc6f1ff35de145ac
CristóbalO_Kronos <dondoncronos@gmail.com>
2022-09-06 16:38:07 -0300

    Buttons and Checkbox

--- app/src/main/java/cl/ucn/disc/dsm/clasesdsm/FirstFragment.java
+++ app/src/main/java/cl/ucn/disc/dsm/clasesdsm/FirstFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import cl.ucn.disc.dsm.clasesdsm.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    private EditText txt_number_1, txt_number_2, txt_resp;
+
+    private RadioButton radioButton1, radioButton2, radioButton3, radioButton4;
+
+    private RadioGroup radioGroup;
 
     @Override
     public View onCreateView(
@@ -22,7 +31,17 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        txt_number_1 = (EditText) binding.textNumber1;
+        txt_number_2 = (EditText) binding.textNumber2;
+        txt_resp = (EditText) binding.txtResp;
+        radioButton1 = (RadioButton) binding.radioButton1;
+        radioButton2 = (RadioButton) binding.radioButton2;
+        radioButton3 = (RadioButton) binding.radioButton3;
+        radioButton4 = (RadioButton) binding.radioButton4;
+
         return binding.getRoot();
+
 
     }
 
@@ -32,10 +51,64 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                if (radioButton1.isChecked()) {
+                    sum();
+                } else if (radioButton2.isChecked()) {
+                    res();
+                } else if (radioButton3.isChecked()) {
+                    mult();
+                } else if (radioButton4.isChecked()) {
+                    div();
+                } else {
+                    showMessage();
+                }
             }
         });
+
+    }
+
+    public void showMessage() {
+        Toast.makeText(this.getContext(), "No se ha seleccionado ninguna operación", Toast.LENGTH_SHORT).show();
+    }
+
+    public String sum() {
+        double val_1 = Integer.parseInt(txt_number_1.getText().toString());
+        double val_2 = Integer.parseInt(txt_number_2.getText().toString());
+        double sum = val_1 + val_2;
+        String res = String.valueOf(sum);
+        txt_number_1.setText(res); //puede haber un error, tal vez sea necesario cambiar el txt_number_1 por txt-resp
+        return res;
+    }
+
+    public String res() {
+        double val_1 = Integer.parseInt(txt_number_1.getText().toString());
+        double val_2 = Integer.parseInt(txt_number_2.getText().toString());
+        double sum = val_1 - val_2;
+        String res = String.valueOf(sum);
+        txt_number_1.setText(res);
+        return res;
+    }
+
+    public String mult(){
+        double val_1 = Integer.parseInt(txt_number_1.getText().toString());
+        double val_2 = Integer.parseInt(txt_number_2.getText().toString());
+        double sum = val_1 * val_2;
+        String res = String.valueOf(sum);
+        txt_number_1.setText(res);
+        return res;
+    }
+
+    public String div(){
+        double val_1 = Integer.parseInt(txt_number_1.getText().toString());
+        double val_2 = Integer.parseInt(txt_number_2.getText().toString());
+        if(val_2 == 0){
+            Toast.makeText(this.getContext(), "ERROR: División entre cero", Toast.LENGTH_SHORT).show();
+            return "Error";
+        }
+        double sum = val_1 / val_2;
+        String res = String.valueOf(sum);
+        txt_number_1.setText(res);
+        return res;
     }
 
     @Override
